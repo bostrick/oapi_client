271053b7dce4029806db296d4e0708e14dbe0e4b
Bowe Strickland <bowe@redhat.com>
2017-12-02 11:40:30 -0500

    WorldMap uses AsyncAsset

--- src/components/WorldMap/index.jsx
+++ src/components/WorldMap/index.jsx
@@ -1,8 +1,8 @@
+import _ from 'lodash';
 import { Card, CardBlock } from 'reactstrap';
 import ReactTooltip from 'react-tooltip';
 import React from 'react';
 
-import { toJS } from 'mobx';
 import { observer } from 'mobx-react';
 // import PropTypes from 'prop-types';
 
@@ -13,28 +13,45 @@ import {
   Geography,
 } from 'react-simple-maps';
 
-import CountryData from '../../store/CountryData';
+import { schemeCategory20 } from 'd3-scale';
+// import { brighter, darker } from 'd3-color';
+import { color } from 'd3-color';
 
+
+// import CountryData from '../../store/CountryData';
+import AsyncAsset from '../../store/AsyncAsset';
+
+const geoData = new AsyncAsset('world-50m.json');
 
 @observer
 class WorldMap extends React.Component {
 
-  constructor() {
-    super();
-    this.cdata = new CountryData();
+  componentWillMount() {
+    geoData.load();
   }
 
-  componentWillMount() {
-    this.cdata.load();
+  getRegionColor = (ldata, geo) => {
+
+    if (_.isEmpty(ldata)) {
+      return color(schemeCategory20[0]);
+    }
+    // const code = _.get(geo, 'properties.iso_a3');
+    const code = _.get(geo, 'id');
+    const linfo = _.get(ldata, code, {});
+    const i = _.indexOf(this.cdata.regions, linfo.region);
+    if (_.isNil(i)) {
+      console.log('bad geo');
+      console.log(geo);
+      return color(schemeCategory20[0]);
+    }
+    return color(schemeCategory20[i % 20]);
   }
 
   render() {
 
-    const cdata = toJS(this.cdata.countryData);
-    const ldata = toJS(this.cdata.labRegionData);
-
-    console.log(cdata);
-    console.log(ldata);
+    if (!geoData.loaded) {
+      return <h6>loading...</h6>;
+    }
 
     return (
       <div>
@@ -55,28 +72,29 @@ class WorldMap extends React.Component {
   }}
 >
   <ZoomableGroup center={[0, 20]} disablePanning>
-    <Geographies geography="/static/world-50m.json">
-      {(geographies, projection) => geographies.map((geography, i) => geography.id !== 'ATA' && (
+    <Geographies geography={geoData.data}>
+      {(geographies, projection) => geographies.map(geography => geography.id !== 'ATA' && (
         <Geography
-          key={i}
-          data-tip={geography.properties.name}
+          key={geography.id}
+          cacheId={geography.id}
+          data-tip="hello"
           geography={geography}
           projection={projection}
           style={{
             default: {
-              fill: '#ECEFF1',
+              // fill: this.getRegionColor(ldata, geography),
               stroke: '#607D8B',
               strokeWidth: 0.75,
               outline: 'none',
             },
             hover: {
-              fill: '#607D8B',
+              // fill: this.getRegionColor(ldata, geography).brighter(),
               stroke: '#607D8B',
               strokeWidth: 0.75,
               outline: 'none',
             },
             pressed: {
-              fill: '#FF5722',
+              // fill: this.getRegionColor(ldata, geography).darker(),
               stroke: '#607D8B',
               strokeWidth: 0.75,
               outline: 'none',
